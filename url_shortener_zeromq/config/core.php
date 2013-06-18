ce4942932bf8a5d7fdd863ff3e9c52e540929fe5
Barry Steyn <barry.steyn@gmail.com>
2013-06-18 16:14:13 -0400

    Clarified host config

--- url_shortener_zeromq/config/core.php
+++ url_shortener_zeromq/config/core.php
@@ -3,7 +3,7 @@
      * Config variables
      */
 
-    $HOST = "localhost";
+    $HOST = "twayd.com"; //Note: This should be changed to appropriate name of whatever machine is hosting
     $ENCODEWORKERS = 5;
     $DECODEWORKERS = 5;
 ?>
